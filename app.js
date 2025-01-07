2ff2173029655fdbf5923bc681ca6d87cefa529e
Pratik <patilpratikp035@gmail.com>
2025-01-08 00:28:09 +0530

    adding port to env and env to gitignore

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const http = require("http");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const path = require("path");
 
 const socketio = require("socket.io");
